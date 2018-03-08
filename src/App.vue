bd3808408d1d7541ae06610decf2640568cffb90
symphony <lichengyang@aliyun.com>
2018-03-08 20:17:08 +0800

    add vue library and codeMirror with emmet autocomplete and theme

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <div class="link">
+      <h3>Outer Javascript</h3>
+      <input id="out-js" type="text" name="javascript" placeholder="outer javascript file">
+    </div>
+
+    <div class="link">
+      <h3>Outer Css</h3>
+      <input id="out-css" type="text" name="css" placeholder="outer css file">
+    </div>
+
+    <section>
+      <div class="html">
+        <h3>Html</h3>
+        <editor type="html"></editor>
+      </div>
+
+      <div class="css">
+        <h3>Css</h3>
+        <editor type="css"></editor>
+      </div>
+
+      <div class="javascript">
+        <h3>JavaScript</h3>
+        <editor type="javascript"></editor>
+      </div>
+
+      <div class="output" id="output">
+        <h3>Output</h3>
+        <iframe></iframe>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Editor from './components/Editor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Editor
+  },
+  mounted () {
+    console.log('HelloWorld')
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+.html, .css, .javascript, .output {
+  display: inline-block;
+  width: 50%;
+}
+
+.output iframe {
+  height: 300px;
+  width: 100%;
+}
+
+.link {
+  display: inline-block;
+  margin-left: 40px;
+}
+</style>
---
+++ src/components/Editor.vue
@@ -0,0 +1,132 @@
+<template>
+  <div :id="type" ref="editor">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  props: ['type'],
+  mounted () {
+    var that = this;
+    let editor = this.$refs.editor
+    let defaultValue = {
+      html: () => {
+        return `<h1 class="demo">HelloWorld</h1>`
+      },
+      css: () => {
+        return '.demo{color: cyan}'
+      },
+      javascript: () => {
+        return 'console.log("HelloWorld")'
+      }
+    }
+    editor.value = defaultValue[this.type]()
+
+    var codeMirrorConfig = {
+      mode: 'text/html',
+      lineNumbers: true,
+      lineWrapping: true,
+      extraKeys: {'Ctrl-Q': function (cm) {
+        cm.foldCode(cm.getCursor())
+      }},
+      flodGutter: true,
+      gutters: [
+        'CodeMirror-linenumbers',
+        'CodeMirror-foldGutter'
+      ],
+      keyMap: 'sublime',
+      profile: 'xhtml'
+    }
+
+    var editorMode = {
+      html: () => 'text/html',
+      css: () => 'css',
+      javascript: () => 'javascript'
+    }
+
+    codeMirrorConfig.mode = editorMode[this.type]()
+
+    window['editor_' + this.type] = CodeMirror.fromTextArea(editor, codeMirrorConfig)
+
+    emmetCodeMirror(window['editor_' + this.type], {
+      'Tab': 'emmet.expand_abbreviation_with_tab',
+      'Cmd-Alt-B': 'emmet.balance_outward'
+    })
+
+    window['editor_' + this.type].setOption('theme', 'dracula')
+
+    window['editor_' + this.type].on('change', function () {
+      that.render()
+    })
+  },
+  methods: {
+    render () {
+      let baseTpl = `
+      <!doctype html>
+      <html>
+      <head>
+      <meta charset="utf8">
+      <title>Playground</title>
+      </head>
+      <body>
+      </body>
+      </html>
+      `
+
+      let sourceCode = () => {
+        let html = window.editor_html.getValue()
+        let css = window.editor_css.getValue()
+        let javascript = window.editor_javascript.getValue()
+        let src = ''
+
+        /**
+         * get outer css and js value
+         */
+        let cssInputVal = document.querySelector('#out-css').value
+        let jsInputVal = document.querySelector('#out-js').value
+
+        /**
+         * parse outer link to html element
+         */
+        let outerCss = `<link rel="stylesheet" href="${cssInputVal}">`
+        let outerJs = `<script src="${jsInputVal}"><\/script>`
+
+        /**
+         * parse the source code you write to html element
+         */
+
+        javascript = '<script>' + javascript + '<\/script>'
+
+        /**
+         * replace elements to html
+         */
+        src = src.replace('</head>', css + outerCss + '</head>')
+        src = baseTpl.replace('</body>', html + '</body>')
+        src = src.replace('</body>', javascript + outerJs + '</body>')
+
+        return src
+      }
+
+      let render = () => {
+        let source = sourceCode()
+        let iframe = document.querySelector('#output iframe')
+
+        let iframeDocument = iframe.contentDocument
+        iframeDocument.open()
+        iframeDocument.write(source)
+        iframeDocument.close()
+      }
+
+      render()
+    }
+  }
+}
+</script>
+
+<style>
+</style>
